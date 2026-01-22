82de76df893313961208a5fe00ba1cebdbfc1b34
Dim <dimitri.crespo.michailidis@gmail.com>
2026-01-22 12:04:33 +0100

    Ex 5 fini

--- src/script.js
+++ src/script.js
@@ -6,12 +6,19 @@ for (let i = 0; i < 20; i++) {
 
 console.log(A_TableauRandom);
 
-const result = document.getElementById("textContentOutput");
+const O_result = document.getElementById("textContentOutput");
 
 let I_compteur = -1;
 const I_intervale = setInterval(() => {
     I_compteur++;
-    result.innerText = A_TableauRandom[I_compteur].toString();
+    O_result.innerText = A_TableauRandom[I_compteur].toString();
+
+    O_result.classList.remove("bleu", "vert", "orange", "rouge");
+
+    if (A_TableauRandom[I_compteur] <= 0) {O_result.classList.add("bleu")}
+    else if (A_TableauRandom[I_compteur] < 20) {O_result.classList.add("vert")}
+    else if (A_TableauRandom[I_compteur] < 30) {O_result.classList.add("orange")}
+    else {O_result.classList.add("rouge")}
+
     if (I_compteur >= A_TableauRandom.length - 1) clearInterval(I_intervale)
 }, 2000);
-
